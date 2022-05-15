abb7fc3c4f0537d3b003d1587d68ef88acfd3aa6
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-05-15 19:01:49 +0200

    Modify subset
    Former-commit-id: f12b97e707cd787f11946329306e598d83fdd5d6

--- src/subset.py
+++ src/subset.py
@@ -14,16 +14,17 @@ class Subset(Dataset):
     """
 
     def __init__(self, dataset, indices, transform=None):
-        self.dataset = dataset[indices]
+        self.dataset = dataset
+        self.indices = indices
+        self.transform = transform
         # We just use the classes from the source dataset
         self.classes = self.dataset.classes
-        self.transform = transform
 
     def __getitem__(self, idx):
-        sample = self.dataset[idx]
+        sample = self.dataset[self.indices[idx]]
         if self.transform is not None:
             sample = self.transform(sample[0]), sample[1]
         return sample
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.indices)
