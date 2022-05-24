1293d3808730be1bbd10636a8b1ef825f7ea10dd
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-05-24 14:05:25 +0200

    Add experiment builder(util) and data handler (util)
    Former-commit-id: 4ce0e371098286913b73bf4eaae92538918b2bcc

---
+++ src/data_handler.py
@@ -0,0 +1,88 @@
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data.sampler import WeightedRandomSampler
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from sklearn.model_selection import train_test_split
+
+from subset import Subset
+
+def setup_data(hparams):
+    # This somehow makes the performance terrible.
+    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    base_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224))
+    ]
+    )
+
+    train_transform = get_train_transforms(hparams["t"])
+
+    root = os.path.join("/", "space", "derma-data")
+    dataset = datasets.ImageFolder(root, base_transforms)
+
+    train_data_idx, val_data_idx = train_test_split(
+        list(range(len(dataset))), test_size=0.2, stratify=dataset.targets)
+    weights, _ = compute_weights(dataset)
+    train_data = Subset(dataset, train_data_idx, train_transform)
+    val_data = Subset(dataset, val_data_idx)
+    return train_data, val_data, weights
+
+def get_train_transforms(flags=None):
+    if flags is None:
+        # Default transforms
+        transforms = nn.Sequential(
+                        transforms.RandomHorizontalFlip(),
+                        transforms.RandomVerticalFlip(),
+                        transforms.RandomRotation(degrees=(0, 180))
+                    )
+    transforms = nn.Sequential(*build_transform_list(flags))
+
+def build_transform_list(flags):
+    transform_flags = flags.split(",")
+    transforms_list = []
+    for flag in transform_flags:
+        if flag == "r":
+            transform = transforms.RandomRotation(degrees=(0, 180))
+        elif flag == "vflip":
+            transform = transforms.RandomVerticalFlip()
+        elif flag == "hflip":
+            transform = transforms.RandomHorizontalFlip()
+            # add new cases here
+        transforms_list.append(transform)
+    return transforms_list
+
+def compute_weights(dataset):
+    class_sample_count = np.unique(dataset.targets, return_counts=True)[1]
+    weights = 1.0 / class_sample_count
+    weights_per_sample = np.array([weights[t] for t in dataset.targets])
+    print(class_sample_count)
+    print(weights_per_sample)
+    return torch.from_numpy(weights).float(), torch.from_numpy(weights_per_sample).float()
+
+def setup_data_loaders(train_data, val_data, batch_size):
+    _, weights_per_sample = compute_weights(train_data.dataset)
+    weights_per_sample = weights_per_sample[train_data.indices]
+    # Test oversampling with factor 1.5
+    weighted_sampler = WeightedRandomSampler(
+        weights=weights_per_sample, num_samples=int(len(train_data) * 1.5))
+    train_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               sampler=weighted_sampler,
+                                               num_workers=8,
+                                               drop_last=False,
+                                               timeout=30000,
+                                               pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(val_data,
+                                             batch_size=batch_size,
+                                             num_workers=8,
+                                             drop_last=False,
+                                             shuffle=False,
+                                             timeout=30000,
+                                             pin_memory=True)
+    return train_loader, val_loader
---
+++ src/experiment_builder.py
@@ -0,0 +1,98 @@
+import glob
+import os
+import glob
+
+import torchvision.models as models
+import torch.nn as nn
+
+from model import Classifier
+
+
+class ExperimentBuilder:
+
+    def __init__(self,
+                 extractor_type,
+                 loss,
+                 num_classes=8,
+                 learning_rate=1e-3,
+                 weight_decay=1e-8,
+                 class_weights=None
+                 ):
+        self.extractor_type = extractor_type
+        self.loss_name = loss
+        self.num_classes = num_classes
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.class_weights = class_weights
+
+        layers = self.get_model.children()
+        convolution_layers = layers[:-1]
+        self.extractor = nn.Sequential(*convolution_layers)
+        self.classifier = nn.Linear(
+            layers[-1][1].in_features, self.num_classes)
+        self.loss = self.configure_loss()
+
+    def create(self, checkpoint):
+        if(checkpoint is None):
+            model = Classifier(
+                extractor=self.extractor,
+                classifier=self.classifier,
+                loss=self.loss,
+                num_classes=self.num_classes
+                learning_rate=self.learning_rate,
+                weight_decay=self.weight_decay,
+            )
+        else:
+            model = self.load_checkpoint(checkpoint)
+
+        return model
+
+    def load_checkpoint(self, checkpoint):
+        if(len(checkpoint) == 0):
+            ckpt = max(glob.glob('./checkpoints/*.ckpt'), key=os.path.getctime)
+        else:
+            ckpt = max(
+                glob.glob(f"./checkpoints/*{checkpoint}.ckpt"), key=os.path.getctime)
+        model = Classifier.load_from_checkpoint(ckpt,
+                                                extractor=self.extractor,
+                                                classifier=self.classifier,
+                                                loss=self.loss,
+                                                num_classes=self.num_classes
+                                                learning_rate=self.learning_rate,
+                                                weight_decay=self.weight_decay,
+                                                )
+        return model
+
+    def get_model(self, pretrained=True):
+        if self.extractor_type == "efficientnet_b1":
+            return models.efficientnet_b1(pretrained)
+        if self.extractor_type == "efficientnet_b2":
+            return models.efficientnet_b2(pretrained)
+        if self.extractor_type == "efficientnet_b3":
+            return models.efficientnet_b3(pretrained)
+        if self.extractor_type == "efficientnet_b4":
+            return models.efficientnet_b4(pretrained)
+        if self.extractor_type == "efficientnet_b5":
+            return models.efficientnet_b5(pretrained)
+        if self.extractor_type == "efficientnet_b6":
+            return models.efficientnet_b6(pretrained)
+        if self.extractor_type == "efficientnet_b7":
+            return models.efficientnet_b7(pretrained)
+        if self.extractor_type == "resnet50":
+            return models.resnet50(pretrained)
+        if self.extractor_type == "resnet101":
+            return models.resnet101(pretrained)
+        if self.extractor_type == "densenet121":
+            return models.densenet121(pretrained)
+        if self.extractor_type == "resnext50_32x4d":
+            return models.resnext50_32x4d(pretrained)
+        if self.extractor_type == "resnext101_32x8d":
+            return models.resnext101_32x8d(pretrained)
+        # Default:
+        return models.efficientnet_b0(pretrained)
+
+    def configure_loss(self):
+        if not self.class_weights is None:
+            return nn.CrossEntropyLoss(weight=self.class_weights)
+        # Default to CE loss
+        return nn.CrossEntropyLoss()
