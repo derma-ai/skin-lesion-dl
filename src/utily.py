10ac8e531890d986a9d1a5f43e4447b176eb02f8
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-05-30 13:46:56 +0200

    Fix
    Former-commit-id: a47c34c82d27ed6841f4fd656553a5404c393875

--- src/utily.py
+++ src/utily.py
@@ -95,11 +95,12 @@ def compute_loader(dataset):
 
     for batch_idx, batch in enumerate(loader):
         if batch_idx == 0:
-            print(batch.shape)  
+            print(batch)
+            print(batch[0].shape)  
         mean += batch[0].mean(dim=[0,2,3])
     mean = mean / len(dataset)
     for batch_idx, batch in enumerate(loader):
-        variance +=  (batch - mean).pow(2).sum(dim=[1,2]) / pixels_per_channel
+        variance +=  (batch[0] - mean).pow(2).sum(dim=[1,2]) / pixels_per_channel
     variance = variance / len(dataset)
     return mean, variance
 
