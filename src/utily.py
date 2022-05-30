f4271d342b2189842872e88e648a8666328161ed
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-05-30 14:59:03 +0200

    Fix
    Former-commit-id: a2b6bb4df6a5528cb2694967fb329fe332e91249

--- src/utily.py
+++ src/utily.py
@@ -91,7 +91,7 @@ def compute_simple(dataset):
 def compute_loader(dataset):
     mean = torch.zeros(3)
     variance = torch.zeros(3)
-    loader = DataLoader(dataset, batch_size=64, num_workers=16)
+    loader = DataLoader(dataset, batch_size=64, num_workers=32)
     pixels_per_channel = dataset[0][0].shape[1] * dataset[0][0].shape[2]
 
     for batch_idx, batch in enumerate(loader):
@@ -105,7 +105,7 @@ def compute_loader(dataset):
 def compute_loader_gpu(dataset, device):
     mean = torch.zeros(3).to(device)
     variance = torch.zeros(3).to(device)
-    loader = DataLoader(dataset, batch_size=64 * 8, num_workers=16)
+    loader = DataLoader(dataset, batch_size=64 * 8, num_workers=32)
     pixels_per_channel = dataset[0][0].shape[1] * dataset[0][0].shape[2]
 
     for batch_idx, (x, y) in enumerate(loader):
