f45ec6cde87dc52688ba3ff2fffe4d929c84be13
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-05-30 11:49:24 +0200

    Add basic analysis
    Former-commit-id: fad1e40db6697ef0b70deead41073b3bfc4a0f5e

--- src/utily.py
+++ src/utily.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
@@ -14,3 +15,30 @@ def compute_existing_resolutions():
         if not image.shape in resolution_sizes:
             resolution_sizes.append(image.shape)
     print(resolution_sizes)
+
+
+
+def main():
+    dataset = setup_dataset()
+    samples_per_class = compute_samples_per_class(dataset)
+    print(samples_per_class)
+    print(dataset.class_to_idx)
+
+
+def setup_dataset():
+    base_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224))
+    ]
+    )
+
+    root = os.path.join("/", "space", "derma-data")
+    dataset = datasets.ImageFolder(root, base_transforms)
+    return dataset
+
+def compute_samples_per_class(dataset):
+    class_sample_count = np.unique(dataset.targets, return_counts=True)[1]
+    return class_sample_count
+
+if __name__ == "__main__":
+    main()
