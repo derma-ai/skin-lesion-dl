d4984bb1d95764cae5f8267f66ad36e1beb66550
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-06-09 18:03:40 +0200

    Add LR finder flag, default true
    Former-commit-id: 9971e066e478bb6a3d84cdc55857ad0367f86c47

--- src/train.py
+++ src/train.py
@@ -50,7 +50,7 @@ def train(gpu,
     
     if hparams["lrf"] == 1:
         # Run learning rate finder
-        lr_finder = trainer.tuner.lr_find(model, train_dataloaders=train_loader, val_dataloaders=val_loader, early_stop_threshold=3.0, max_lr=1e-3)
+        lr_finder = trainer.tuner.lr_find(model, train_dataloaders=train_loader, val_dataloaders=val_loader, early_stop_threshold=3.0, max_lr=3e-4)
         new_lr = lr_finder.suggestion()
         hparams["lr"] = new_lr
         model.hparams["lr"] = new_lr
