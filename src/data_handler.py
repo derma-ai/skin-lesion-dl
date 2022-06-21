7733ffcc715d1a10c06c14be1f411eca3529579a
Lisa Vivien Podszun <podszun@cmpsrv01.ciip.in.tum.de>
2022-06-21 23:29:08 +0200

    fix implementation
    Former-commit-id: 338ba0051926b62f6846e1148706feb2c909d338

--- src/data_handler.py
+++ src/data_handler.py
@@ -70,7 +70,7 @@ def compute_weights(dataset, relatively_weighted):
     return torch.from_numpy(weights).float(), torch.from_numpy(weights_per_sample).float()
 
 
-def setup_data_loaders(train_data, val_data, batch_size, over_sampling_rate, relatively_weighted):
+def setup_data_loaders(train_data, val_data, batch_size, over_sampling_rate, relatively_weighted=True):
     if (over_sampling_rate <= 1):
         train_loader = torch.utils.data.DataLoader(train_data,
                                                 batch_size=batch_size,
