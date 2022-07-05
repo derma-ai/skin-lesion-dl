4696162f7e9ab20c73b9ab7f91ff3cc3d29eb602
Lisa Vivien Podszun <podszun@cmpsrv01.ciip.in.tum.de>
2022-07-05 21:27:11 +0200

    combine weighted sampling flags
    Former-commit-id: d07e47570e8d9774249d67102e94bce211be60aa

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # The user provides the model to train with a BS of 32 and 64
-python ./src/train.py -e 60 -b 32 -t "r,hflip,vflip" -osr 1.5 -l "wce" -ex "Tests for data augmentation/preprocessing" -m $1 -gpu $2
-#python ./src/train.py -e 60 -b 64 -t "r,hflip,vflip" -osr 1.5 -l "wce" -ex "Weighted sampling with 1.5 osr, weighted loss, no normalization, transforms" -m $1 -gpu $2
+python ./src/train.py -e 60 -b 32 -t "r,hflip,vflip" -osr 1.5 -l "wce" -ws 0 -ex "Without weighted sampling" -m "efficientnet_b0" -gpu $1
+python ./src/train.py -e 60 -b 32 -t "r,hflip,vflip" -osr 1.5 -l "wce" -ws 3 -ex "Weighted sampling with 1.5 osr" -m "efficientnet_b0" -gpu $1
