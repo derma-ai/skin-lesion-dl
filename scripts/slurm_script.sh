bfd504401bf62c1ac63eaeb53b76d2696d9794af
Lisa Vivien Podszun <podszun@cmpsrv01.ciip.in.tum.de>
2022-09-12 13:25:18 +0200

    adapt slurm_script
    Former-commit-id: dccc668014b667ad6c7df070c5438a0f3b1685df

--- scripts/slurm_script.sh
+++ scripts/slurm_script.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -l
+#!/bin/bash
 
-#SBATCH --job-name=weighted_focal
+#SBATCH --job-name=processing-experiment
 #SBATCH --output=res.txt
 #SBATCH --error=res.err
 
@@ -10,5 +10,7 @@
 
 CUDA_VISIBLE_DEVICES=0
 
+source /u/home/podszun/.bashrc
 source activate idp_env
-srun ./train.sh efficientnet_b0 64 224
+srun which python
+srun train.sh
